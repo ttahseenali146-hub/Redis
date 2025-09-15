4461378a2dfe786be0055102e2e16f9a04be36b2
ttahseenali146-hub <ttahseenali146@gmail.com>
2025-09-15 12:11:27 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 import express from "express";
-import dotenv from "dotenv";
-import UserRoute from "./Routes/user.js";
-import { connectRedis } from "./Utils/redis.js";
-
-dotenv.config();
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
-// Connect to Redis
-connectRedis();
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Realtime Socket Chat Server</h1>");
+});
 
-app.use("/user", UserRoute);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  // Join a room
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+  });
+  socket.on("leave", (roomId) => {
+    socket.leave(roomId);
+  });
 
-app.listen(5050, () => {
-  console.log("Server is running on port 5050");
+//   Broadcast to room 
+  socket.on("send", (message) => {
+    console.log(message)
+    socket.to(message.room).emit("message", message);
+  });
+});
+
+server.listen(5050, () => {
+  console.log("listening on *:5050");
 });
